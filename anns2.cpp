2775f30ab00c1d6ccda963f9f06f882db8f24994
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-22 02:11:14 +0530

    c++

---
+++ anns2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Enter the number = " << "\n";
+    int number_1 ;
+    cin >> number_1;
+    
+    cout << " Enter the number  " << "\n";
+    int number_2 ;
+    cin >> number_2 ;
+
+    int sum = number_1 + number_2;
+
+    cout << " Sum = " << sum << "\n";
+    
+    
+    
+    
+    return 0;
+}
